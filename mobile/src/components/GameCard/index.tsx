709efb1428647f3fa6e61dc5d13deb7984b92d39
Nicoli <nickstefan4@gmail.com>
2022-09-18 20:32:15 -0300

    Atualizada mobile

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -5,9 +5,11 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
     id: string;
-    name: string;
-    ads: string;
-    cover : ImageSourcePropType;
+    title: string;
+    _count: {
+      ads: number;
+    };
+    bannerUrl : string;
 
 }
 
@@ -20,7 +22,7 @@ export function GameCard({data}:Props) {
     <TouchableOpacity style={styles.container}>
         <ImageBackground
         style={styles.cover}
-        source={data.cover}
+        source={{uri: data.bannerUrl}}
         >
 
         <LinearGradient 
@@ -28,11 +30,11 @@ export function GameCard({data}:Props) {
         style={styles.footer}
         >
          <Text style={styles.name}>
-            {data.name}
+            {data.title}
          </Text> 
 
          <Text style={styles.ads}>
-            {data.ads} anúncios
+            {data._count.ads} anúncios
          </Text>  
         </LinearGradient>
         </ImageBackground>
